509708c2d7e8581fbc86fdf4527c175195bb6f66
suhanipandey04 <dheeverse@gmail.com>
2025-11-15 21:15:59 +0530

    problem 13

---
+++ remove_all_occurences_of_substring.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+string removeOccurrences(string s, string part) {
+    while(s.length() != 0 && s.find(part) < s.length()) {
+        s.erase(s.find(part), part.length());
+    }
+    return s;
+}
+
+int main() {
+    string s = "daabcbaabcbc";
+    string part = "abc";
+    
+    string result = removeOccurrences(s, part);
+    cout << "String after removing Occurrences: " << result << endl;
+
+    return 0;
+}
